ffe3bf26f7f5ef93df569f5f7040f574367b1c53
Maturi. Harsha vardhan <134041146+maturinorv2004@users.noreply.github.com>
2024-06-25 10:28:52 +0530

    Simple Calculator.py
    calculator.py Description
    Imports and Dependencies
    
    Imports necessary libraries such as math for advanced calculations.
    Basic Arithmetic Functions
    
    Functions for addition, subtraction, multiplication, and division.
    
    Advanced Mathematical Functions
    
    Functions for square root, exponentiation, and trigonometric calculations.
    
    User Interface (Command Line)
    
    Command-line interface to interact with the user, taking input and displaying results.

---
+++ Simple Calculator.Py.py
@@ -0,0 +1,36 @@
+from tkinter import *
+class Calculator:
+    def __init__(self, cal):
+        cal.title("ADV Calculator")
+        font, calbtn, btnArray, operator = ("arial",20, "bold"), "789+456-123*0C=/", [], ""
+        text_input = IntVar()
+        def BtnClick(number):
+            nonlocal operator
+            operator = operator+str(number)
+            text_input.set(operator)
+        def Equal():
+            nonlocal operator
+            calculated = eval(operator)
+            text_input.set(calculated)
+            operator = ""
+        def ClearEntry():
+            nonlocal operator
+            text_input.set(0)
+            operator =""
+
+        Entry(cal, textvariable=text_input, font=font, bg="powder blue", bd=30, justify="right").grid(columnspan=4)
+        index = 0
+        for row in range(4):
+            for column in range(4):
+                btnArray.append(Button(cal, text=calbtn[index], bg="powder blue", padx=16, pady=16, bd=8, font=font))
+                btnArray[index].grid(row=row+1, column=column)
+                if calbtn[index] == "=":
+                    btnArray[index]["command"] = Equal
+                elif calbtn[index] == "C":
+                    btnArray[index]["command"] = ClearEntry
+                else:
+                    btnArray[index]["command"] = lambda x = calbtn[index] : BtnClick(x)
+                index+=1
+        cal.mainloop()
+cal = Tk()
+Calculator(cal)
